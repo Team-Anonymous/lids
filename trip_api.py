11e93d420f91aaca00b9f7cca09457d9f13f19d4
Sohail Chamadia <sohailyc@gmail.com>
2017-04-01 14:15:31 +0530

    Rename and API update

--- trip_api.py
+++ trip_api.py
@@ -8,7 +8,6 @@ def send_loc():
 			location=loc()
 			latitude=(location[0])
 			longitude=(location[1])
-#			print(" "+latitude+" "+longitude)
 			if latitude != "0.0" and longitude != "0.0":
 				userdata = {'vehicle_id': "106", 'latitude': latitude, 'longitude': longitude}
 				resp = requests.post('http://license.esy.es/insert.php', data=userdata,timeout=10)
